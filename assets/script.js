76994feafdad3888ac55737b977cf77cdb31bf4b
moonflower <tayler.brianne.baldwin@gmail.com>
2023-12-09 20:40:53 -0500

    Building html, styling css, and starting js

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+
+var myQuestions = [
+    {
+      question: "_______ allows us to write JavaScript inside the HTML, and add a source attribute to connect the external JavaScript.",
+      answers: {
+        a: "Script tag",
+        b: "Console.log",
+        c: "Variables"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "_______ is a value that is either true or false.",
+      answers: {
+        a: "String",
+        b: "Number",
+        c: "Boolean"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "_______ are used to store groups in a single variable.",
+      answers: {
+        a: "Arrays",
+        b: "Conditions",
+        c: "Methods",
+      },
+      correctAnswer: "a"
+    }
+  ];
+
