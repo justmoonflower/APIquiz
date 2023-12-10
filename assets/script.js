33c641fe6fb66360c9e232ecf197653d03bd7e09
moonflower <tayler.brianne.baldwin@gmail.com>
2023-12-10 12:48:30 -0500

    hide start when quiz begins

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
+const startButton = document.getElementById("start-button")
+const questionContainerElement= document.getElementById ("question-container")
 
+startButton.addEventListener("click", startGame)
 
+function startGame() {
+  console.log("Started")
+  startButton.classList.add("hide")
+  questionContainerElement.classList.remove("hide")
+}
 
+function setNextQuestion() {
 
+}
+
+function selectAnswer() {
+
+}
 
 
 
