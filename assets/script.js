dacf5f9dc491110a4c0752ecbec7186fcb314a61
moonflower <tayler.brianne.baldwin@gmail.com>
2023-12-10 12:31:05 -0500

    styling for buttons, changing js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 var questions = [
 // First question and answer set
   {
@@ -29,67 +36,3 @@ var questions = [
     ]
   }
 ];
-// Create Variables for element ID's
-var questionElement = document.getElementById("question");
-var answerButtons = document.getElementById("answers");
-var nextButton = document.getElementById("next-button");
-
-//Displaying questions with question number after append
-let currentQuestionIndex = 0;
-let score = 0;
-
-function startQuiz(){
-  currentQuestionIndex = 0;
-  score = 0;
-  nextButton.innerHTML = "Next";
-  showQuestion();
-}
-
-function showQuestion(){
-  resetState();
-  let currentQuestion = questions[currentQuestionIndex];
-  let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + "." + currentQuestion.question;
-
-  // Will retrive anwers from var questions, create buttons for them 
-  currentQuestion.answers.forEach(answer => {
-    var button = document.createElement("button");
-    button.innerHTML = answer.text;
-    button.classList.add("button");
-    // Appending to HTML
-    answerButtons.appendChild(button);
-
-    if(answer.correct){
-      button.datasetset.correct = answer.correct;
-    }
-    button.addEvenetListener("click", selectAnswer);
-  });
-}
-function resetState(){
-  nextButton.style.display = "none";
-  while(answerButtons.firstChild){
-    answer.Buttons.removeChild(answerButtons.firstChild);
-  }
-}
-
-function selectAnswer(e){
-  var selectedBtn = e.target;
-  var isCorrect = selectedBtn.dataset.correct === "true";
-  if(isCorrect){
-    selectedBtn.classList.add("correct");
-  }else{
-    selectedBtn.classList.add("incorrect");
-  }
-  Array.from(answerButtons.children).forEach(button => {
-    if(button.dataset.correct === "true"){
-      button.classList.add("correct");
-    }
-      button.disabled = true;
-  });
-  nextButton.style.display = "block";
-  }
-
-
-
-
-startQuiz();
